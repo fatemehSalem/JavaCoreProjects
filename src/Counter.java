8a45c90168bf9ec965bc0a4fb2140525dbb59883
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-04-15 10:21:58 +0330

    how race condition works with mutual exclusive

---
+++ src/Counter.java
@@ -0,0 +1,6 @@
+public class Counter {
+    int counter;
+    public  void increment(){
+        counter++;
+    }
+}
--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,15 +1,17 @@
 
 public class RunnableClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+    Counter c = new Counter();
     Runnable classA = () -> {
-            for( int i = 0 ; i <= 5 ; i++){
-                System.out.println("hello from A");
+            for( int i = 1 ; i <= 1000 ; i++){
+                c.increment();
             }
     };
 
     Runnable classB = () -> {
-            for( int i = 0 ; i <= 5 ; i++){
-                System.out.println("hello from B");
+            for( int i = 1 ; i <= 1000 ; i++){
+                c.increment();
+
             }
         };
 
@@ -19,5 +21,10 @@ public class RunnableClass {
 
     t1.start();
     t2.start();
+
+    t1.join();
+    t2.join();
+
+        System.out.println( c.counter);
     }
 }
