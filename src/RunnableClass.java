e9e9ffd950d9f360d200bdd2f2c264432aed0884
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-04-21 11:57:12 +0330

    Linear Search. o(n)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -21,6 +21,7 @@ public class RunnableClass {
         return numbers;
     }*/
 
+
     public static int linearSearch(int[] nums, int target) {
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i] == target)
