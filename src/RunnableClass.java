cfaf17391457bfb7ae23e9a61c6a3d2ed208a068
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-04-23 11:26:08 +0330

    Selection Sort: O(n2)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -3,25 +3,34 @@
 public class RunnableClass {
     public static void main(String[] args) {
         int[] nums = {2 , 5 , 4 , 11 , 1 , 7 , 10 , 13 , 12};
-        int temp;
 
         System.out.println("before sorting:");
         for(int num : nums)
             System.out.print(num + " ");
 
-        for(int i = 0 ; i < nums.length; i++){
-            for(int j = 0 ; j < nums.length - i - 1 ; j++){
-                if(nums[j] > nums[j+1]){
-                    temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
-            }
-        }
+        int[] sortedNums = selectionSort(nums);
         System.out.println();
         System.out.println("after sorting:");
-        for(int num : nums)
-            System.out.print(num + " ");
+        for(int s : sortedNums)
+            System.out.print(s + " ");
     }
+    public static int[]  selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            // for (int i = arr.length - 1; i >= 0; i--) { //Find the index of the maximum element
+            int minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                //if (nums[j] < nums[minIndex]) { //find the minimum element, sort from lower to upper , ASC
+                if (nums[j] > nums[minIndex]) { //sort from upper to lower, DESC
+                    minIndex = j;
+                }
+            }
 
+            if (minIndex != i) {
+                int temp = nums[i];
+                nums[i] = nums[minIndex];
+                nums[minIndex] = temp;
+            }
+        }
+        return nums;
+    }
 }
