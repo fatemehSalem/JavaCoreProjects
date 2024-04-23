c11d2d5d1e4e58cfb8cd5b5224434d51f4bd9e3a
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-04-23 08:46:35 +0330

    Bubble Sort: O(n2)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -2,55 +2,26 @@
 
 public class RunnableClass {
     public static void main(String[] args) {
-        int[] nums = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10};
-        int target = 100;
+        int[] nums = {2 , 5 , 4 , 11 , 1 , 7 , 10 , 13 , 12};
+        int temp;
 
-       // int result2 = linearSearch(nums , target);
-        int result1 = binarySearch(nums , target , 0 , nums.length - 1);
+        System.out.println("before sorting:");
+        for(int num : nums)
+            System.out.print(num + " ");
 
-      if(result1 != -1)
-            System.out.println("Element found at Index: " + result1);
-        else
-            System.out.println("Element not found!");
-    }
-/*    public static int[] createRandomArray(int size) {
-        Random random = new Random();
-        int[] numbers = new int[size];
-        for (int i = 0; i < size; i++) {
-            numbers[i] = random.nextInt();
-        }
-        return numbers;
-    }*/
-
-    public static int linearSearch(int[] nums, int target) {
-        int steps = 0;
-        for(int i = 0 ; i < nums.length ; i++){
-            steps ++;
-            if(nums[i] == target){
-                System.out.println("Steps taken by Linear: " + steps);
-                return i;
+        for(int i = 0 ; i < nums.length; i++){
+            for(int j = 0 ; j < nums.length - 1 ; j++){
+                if(nums[j] > nums[j+1]){
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
             }
-
         }
-        System.out.println("Steps taken by Linear: " + steps);
-        return -1;
+        System.out.println();
+        System.out.println("after sorting:");
+        for(int num : nums)
+            System.out.print(num + " ");
     }
 
-    public static int binarySearch(int[] nums, int target , int left , int right) {
-        while(left <= right){
-            int mid = (left + right) / 2;
-            if(nums[mid] == target){
-
-                return mid;
-            } else if(nums[mid] < target){
-                left = mid + 1;
-                binarySearch(nums , target , left , right);
-            } else{
-                right = mid - 1;
-                binarySearch(nums , target , left , right);
-            }
-
-        }
-        return -1;
-    }
 }
