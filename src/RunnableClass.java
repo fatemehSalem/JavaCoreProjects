88c22eb5945233263d58a0f96f7a7fd2a7051089
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-04-29 09:30:48 +0330

    Time complexity: O(n^2)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -4,35 +4,22 @@ import java.util.ArrayList;
 public class RunnableClass {
 
 
-    public static ArrayList<Integer> reArrangeArray(int[] arr) {
-        ArrayList<Integer> lst = new ArrayList<>();
-        int length = arr.length;
+    public static int numberOfPair(int[] x, int[] y ) {
         int count = 0;
-        if(length % 2 == 0){
-            while(count < (length / 2) ){
-                if( (length - count - 1) != count)
-                    lst.add(arr[length - count - 1 ]);
-                lst.add(arr[count]);
-                count ++;
-
-            }
-        } else{
-            while(count <= (length / 2) ){
-                if( (length - count - 1) != count)
-                    lst.add(arr[length - count - 1 ]);
-                lst.add(arr[count]);
-                count ++;
+        for( int i : x){
+            for( int j : y){
+                if( Math.pow( i ,j ) > Math.pow(j ,i ) ){
+                    count ++;
+                }
             }
         }
-
-
-        return lst;
+        return  count;
     }
     public static void main(String[] args) {
-        int[] arr = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12  };
-
-        ArrayList<Integer> result = reArrangeArray(arr);
-        System.out.println("rearrange array is: " + result);
+        int[] x = {2 , 3 , 4 , 5};
+        int[] y = {1 , 2 , 3};
+        int result = numberOfPair(x , y);
+        System.out.println("number of pairs is: " + result);
     }
 }
 
