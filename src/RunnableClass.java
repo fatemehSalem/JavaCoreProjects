d3ed760321e204fc919d6effcbfc751a33772cd1
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-04-29 07:36:48 +0330

    Time complexity: O(n)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,23 +1,27 @@
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class RunnableClass {
 
 
-    public static int maxMissingNumber(int[] arr) {
-        Arrays.sort(arr);
-
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] + 1 != arr[i + 1])
-                return arr[i] + 1;
+    public static ArrayList<Integer> reArrangeArray(int[] arr) {
+        ArrayList<Integer> lst = new ArrayList<>();
+        int length = arr.length;
+        int count = 0;
+        while(count <= (length / 2) ){
+            if( (length - count - 1) != count)
+                lst.add(arr[length - count - 1 ]);
+                lst.add(arr[count]);
+            count ++;
         }
-        return 0;
+
+        return lst;
     }
     public static void main(String[] args) {
-        int[] arr = { -1 , 2 , 0 , 3};
+        int[] arr = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 };
 
-        int result = maxMissingNumber(arr);
-        System.out.println("missing number is: " + result);
+        ArrayList<Integer> result = reArrangeArray(arr);
+        System.out.println("rearrange array is: " + result);
     }
 }
 
