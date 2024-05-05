8d10aee8093845f217ec6b4c5dfb22204db46dab
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-05 20:34:41 +0330

    Time complexity: O(n*logn) + O(nlogn) = O(nlogn)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,33 +1,46 @@
+import java.util.Arrays;
+
 public class RunnableClass {
-    public static int trap(int[] arr) {
-        if (arr == null || arr.length <= 2)
-            return 0;
-
-        int n = arr.length;
-        int[] leftMax = new int[n];
-        int[] rightMax = new int[n];
-
-        leftMax[0] = arr[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], arr[i]);
+    public static boolean isPythagoreanTriplet(int[] arr) {
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = arr[i] * arr[i];
         }
+        Arrays.sort(arr);
 
-        rightMax[n - 1] = arr[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
+        for(int i = 0 ; i < arr.length ; i++){
+            if(i != arr.length - 1){
+                int target = arr[i] + arr[i+1];
+                if(binarySearch(arr , target) != -1){
+                    return true;
+                }
+            }
+
         }
-
-        int waterTrapped = 0;
-        for (int i = 0; i < n; i++) {
-            waterTrapped += Math.min(leftMax[i], rightMax[i]) - arr[i];
-        }
-
-        return waterTrapped;
+        return false;
     }
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
     public static void main(String[] args) {
-        int[] arr = {6 , 9 , 9};
-        System.out.println("Amount of water trapped: " + trap(arr));
+        int[] arr = {8, 2, 4, 6, 5 , 12};
+        System.out.println("Is there a Pythagorean triplet? "  + isPythagoreanTriplet(arr));
     }
 
 }
