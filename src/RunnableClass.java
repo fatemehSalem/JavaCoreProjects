62efa8d9b24e450f4bf87bb37f43eadd10556aca
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-05 19:29:06 +0330

    Time complexity: O(N)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,25 +1,25 @@
 public class RunnableClass {
-    public static int trap(int[] height) {
-        if (height == null || height.length <= 2)
+    public static int trap(int[] arr) {
+        if (arr == null || arr.length <= 2)
             return 0;
 
-        int n = height.length;
+        int n = arr.length;
         int[] leftMax = new int[n];
         int[] rightMax = new int[n];
 
-        leftMax[0] = height[0];
+        leftMax[0] = arr[0];
         for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+            leftMax[i] = Math.max(leftMax[i - 1], arr[i]);
         }
 
-        rightMax[n - 1] = height[n - 1];
+        rightMax[n - 1] = arr[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+            rightMax[i] = Math.max(rightMax[i + 1], arr[i]);
         }
 
         int waterTrapped = 0;
         for (int i = 0; i < n; i++) {
-            waterTrapped += Math.min(leftMax[i], rightMax[i]) - height[i];
+            waterTrapped += Math.min(leftMax[i], rightMax[i]) - arr[i];
         }
 
         return waterTrapped;
