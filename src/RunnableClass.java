4ab4c3e44b10c1ed12f1243d55883c89cf43b47b
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-05 18:37:38 +0330

    Time complexity: O(nlogn)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -2,23 +2,39 @@ import java.util.Arrays;
 
 public class RunnableClass {
 
-    public static int[]  reverseArray(int[] arr , int subArrayLength) {
-        int temp ;
-        for(int i = 0 ; i < subArrayLength ; i++){
-            if(arr[i] < arr[i+1]){
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-            }
+    public static void  reverseArray(int[] arr , int low , int k) {
+        if(low < k){
+            int pi = partition(arr , low , k);
+            reverseArray(arr , pi + 1 , k);
         }
+    }
 
-        return  arr;
+    public static int partition(int[] arr , int low , int k){
+        int pi = arr[k];
+        int i = low -1 ;
+
+         for(int j = low ; j < k ; j++){
+             if(arr[j] > pi){
+                 i ++ ;
+                 swap(arr , i , j);
+             }
+         }
+         swap(arr , i + 1 , k);
+         return i+1;
+    }
+
+    public static void swap(int[] arr , int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1 , 3 , 2 , 10 , 13};
-        int[]  minPlatforms = reverseArray(arr , 3);
-        System.out.println("Minimum number of platforms is: " + Arrays.toString(minPlatforms));
+        int[] arr = {10 , 2  , 13 , 7 , 11 , 3 , 9};
+        int k = 5;
+        reverseArray(arr ,  0 ,  k -1);
+        reverseArray(arr ,  k ,  arr.length - 1);
+        System.out.println("Minimum number of platforms is: " + Arrays.toString(arr));
     }
 
 }
