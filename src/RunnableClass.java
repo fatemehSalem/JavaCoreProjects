155dec7671d845eb34aabaacf21efae5c045a4c0
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-06 07:07:58 +0330

    Time complexity: O(n*logn)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,46 +1,27 @@
 import java.util.Arrays;
 
 public class RunnableClass {
-    public static boolean isPythagoreanTriplet(int[] arr) {
-        for(int i = 0 ; i < arr.length ; i++){
-            arr[i] = arr[i] * arr[i];
-        }
+    public static int[] zigZag(int[] arr) {
+        int n = arr.length, mid = n / 2 , increment = 1 ;
+
         Arrays.sort(arr);
 
-        for(int i = 0 ; i < arr.length ; i++){
-            if(i != arr.length - 1){
-                int target = arr[i] + arr[i+1];
-                if(binarySearch(arr , target) != -1){
-                    return true;
-                }
-            }
+        int[] tempArray = new int[n];
 
-        }
-        return false;
-    }
-    public static int binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) {
-                return mid;
-            }
-
-            if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        for(int i = 0 ; i < mid ; i++){
+            tempArray[2 * i] = arr[i];
         }
 
-        return -1;
+        for(int k = n - 1 ; k >= mid ; k--){
+            tempArray[increment] = arr[k];
+            increment = increment +2;
+        }
+        return tempArray;
     }
+
     public static void main(String[] args) {
-        int[] arr = {20, 2, 4, 6, 5 , 12 , 99 , 101};
-        System.out.println("Is there a Pythagorean triplet? "  + isPythagoreanTriplet(arr));
+        int[] arr = {4, 3, 7, 8, 6, 2, 1};
+        System.out.println("Is there a Pythagorean triplet? "  + Arrays.toString(zigZag(arr)));
     }
 
 }
