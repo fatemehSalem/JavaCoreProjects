45967e7b34f1a0c6b04cb6af99cd7816c27247f9
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-07 11:53:50 +0330

    Time complexity: O(n^2)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,23 +1,33 @@
 
 public class RunnableClass {
-    public static void main(String[] args) {
-    Runnable classA = () -> {
-            for( int i = 0 ; i <= 5 ; i++){
-                System.out.println("hello from A");
-            }
-    };
+    public static String findIndexes(int[] arr , int s){
+        String result = null;
+        for(int i = 0 ; i < arr.length ; i++){
+            int sum = 0;
+           for(int j = i + 1 ; j < arr.length ; j++){
+               if(j == i + 1)
+                    sum = sum + arr[i] + arr[j];
+               else
+                   sum = sum + arr[j];
+               if(sum > s)
+                   break;
+               else if(sum == s){
+                   result =(i + 1) + "" + (j + 1) + "";
+                   return result;
+               }
 
-    Runnable classB = () -> {
-            for( int i = 0 ; i <= 5 ; i++){
-                System.out.println("hello from B");
-            }
-        };
-
-
-    Thread t1 = new Thread(classA);
-    Thread t2 = new Thread(classB);
-
-    t1.start();
-    t2.start();
+           }
+        }
+        return result;
     }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int s = 15;
+        String result = findIndexes(arr , s);
+        if(result != null)
+            System.out.println("The sum of elements from " + result.charAt(0) + "st position to " + result.charAt(1) + "st position");
+        else
+            System.out.println("The result has not been found!");
+    }
+
 }
