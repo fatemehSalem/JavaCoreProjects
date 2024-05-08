11006637df059d9d7060709f6b71159e11195152
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-08 08:48:24 +0330

    Time complexity: O(N^2)*O(logN) = O(n^2 LogN)

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,46 +1,36 @@
 import java.util.Arrays;
 
 public class RunnableClass {
-    public static boolean isPythagoreanTriplet(int[] arr) {
-        for(int i = 0 ; i < arr.length ; i++){
-            arr[i] = arr[i] * arr[i];
-        }
+    public static int countTriplets(int[] arr) {
+        int sum, count = 0;
         Arrays.sort(arr);
-
         for(int i = 0 ; i < arr.length ; i++){
-            if(i != arr.length - 1){
-                int target = arr[i] + arr[i+1];
-                if(binarySearch(arr , target) != -1){
-                    return true;
+            for(int j = i+1 ; j < arr.length ; j++){
+                    sum = arr[i] + arr[j];
+                if (binarySearch(arr, sum) != -1) {
+                    count++;
                 }
             }
-
         }
-        return false;
+        return count;
+      
     }
-    public static int binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) {
-                return mid;
+    public static int binarySearch(int[] arr, int x) {
+            int left = 0, right = arr.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] == x)
+                    return mid;
+                if (arr[mid] < x)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
             }
-
-            if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            return -1;
         }
-
-        return -1;
-    }
     public static void main(String[] args) {
-        int[] arr = {20, 2, 4, 6, 5 , 12 , 99 , 101};
-        System.out.println("Is there a Pythagorean triplet? "  + isPythagoreanTriplet(arr));
+        int[] arr = {1, 5, 3, 2};
+        System.out.println("There are " + countTriplets(arr) + " triplets");
     }
 
 }
