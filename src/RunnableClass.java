9996db2e42b279bd559e81ca1245a3c5010ff879
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-11 16:58:01 +0330

    Comparator

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,23 +1,30 @@
-import com.sun.source.tree.Tree;
-
 import java.util.*;
 
 public class RunnableClass {
 
     public static void main(String[] args)
     {
-        HashMap<String , Integer> hashMap = new HashMap<>();
-        hashMap.put("Ali" , 1);
-        hashMap.put("Mohammad-Javad" , 2);
-        hashMap.put("Mohammad-Mahdi" , 3);
+        List<Integer> list = new ArrayList<>();
 
-        hashMap.putIfAbsent("Mohammad-Mahdi" , 16);
+        list.add(52);
+        list.add(21);
+        list.add(20);
+        list.add(55);
+        list.add(51);
 
-        hashMap.put("Mohammad-Mahdi" , 16);
+        list.sort((o1, o2) -> {
+            if (o1 % 10 > o2 % 10)
+                return 1;
+            else if(o1 % 10 == o2 % 10)
+                return 0;
+            else
+                return -1;
+        });
 
-        for(String key : hashMap.keySet()){
-            System.out.println(key + " : "  + hashMap.get(key)  );
+        for(Integer i : list){
+            System.out.println(i);
         }
+
     }
 }
 
