c2256df46c12c872070576bb9bdabf6dbb329bfd
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-05-19 11:08:46 +0330

    new feature of JAVA 17: sealed calsses

---
+++ src/Shape.java
@@ -0,0 +1,4 @@
+public sealed class Shape permits Circle, Rectangle, CustomShape {
+    // Common properties and methods for all shapes
+}
+
---
+++ src/Triangle.java
@@ -0,0 +1,4 @@
+// This class can extend CustomShape because CustomShape is non-sealed
+public class Triangle extends CustomShape {
+    // Triangle-specific properties and methods
+}
