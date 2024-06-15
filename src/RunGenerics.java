2d0b8bc67b1f8f4db950f974802c2885897c6561
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-06-15 12:00:34 +0330

    Inter Communication between threads

--- src/RunGenerics.java
+++ src/RunGenerics.java
@@ -1,3 +1,8 @@
 public class RunGenerics {
-
+    public static void main(String[] args) {
+        Box<Integer> box = new Box<>(11);
+        Box<String> box2 = new Box<>("hello");
+        System.out.println(box.getItem().getClass().getName());
+        System.out.println(box2.getItem().getClass().getName());
+    }
 }
