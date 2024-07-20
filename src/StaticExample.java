86224bc29607a0799f0b3f83eb6ea5f36612bef9
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-20 18:33:42 +0330

    A static method can only access static data members and static methods of another class or the same class but cannot access non-static methods and variables

---
+++ src/StaticExample.java
@@ -0,0 +1,13 @@
+
+public class StaticExample {
+    private static int Y = 10;
+    private static void printHi(){
+        System.out.println("print hi");
+    }
+    public static void main(String[] args) {
+        String X = "Hi";
+        printHi();
+        System.out.println(Y);
+
+    }
+}
