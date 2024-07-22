54228f96c1c14a5be1a58e1e61e1339a3db094c8
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-22 10:47:10 +0330

    upcasting

--- src/GeneralRun.java
+++ src/GeneralRun.java
@@ -1,6 +1,7 @@
 public class GeneralRun {
     public static void main(String[] args) {
         Parent p = new Child(); //UpCasting
+       // p.showChild(); we dont access to Child's method
         p.showParent();
         p.show();
     }
