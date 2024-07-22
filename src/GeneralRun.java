49c6b62f54c24f1b484437ba36847a5dc2332a23
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-22 13:57:39 +0330

    Concrete Class: Define a concrete method that extends the abstract class and implements the interface

--- src/GeneralRun.java
+++ src/GeneralRun.java
@@ -1,9 +1,8 @@
 public class GeneralRun {
     public static void main(String[] args) {
-        ClassA aClass = new ConcreteClass();
-        aClass.methodB();
-        aClass.methodD();
-        aClass.methodC();
+        MyDog dog = new MyDog();
+        dog.eat();
+        dog.sleep();
 
     }
 }
---
+++ src/MyDog.java
@@ -0,0 +1,11 @@
+public class MyDog extends Mammal{
+    @Override
+    public void sleep() {
+        System.out.println("sleeping...");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
