94ef2e1a63d1fb2d47cdf10f447242db945a1e52
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-23 10:09:29 +0330

    Concrete Class: Define a concrete method that extends the abstract class and implements the interface

--- src/GeneralRun.java
+++ src/GeneralRun.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class GeneralRun {
     public static void main(String[] args) {
-        MyDog dog = new MyDog();
-        dog.eat();
-        dog.sleep();
+        String[] names = {"Alice" , "Sarah" , "Zahra" , "Ali"};
+        Arrays.sort(names, Comparator.naturalOrder());
+        for(String s:names)
+            System.out.println(s);
 
     }
 }
