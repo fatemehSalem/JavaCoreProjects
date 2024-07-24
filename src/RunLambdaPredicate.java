3202c119fcbd7961b6a533e1d19ba2c6fceec85d
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-24 11:55:39 +0330

    predicate and lambda expression

--- src/RunLambdaPredicate.java
+++ src/RunLambdaPredicate.java
@@ -7,13 +7,17 @@ public class RunLambdaPredicate {
         //************* without lambda ****************//
         //arrays can indeed be converted to stream using the Arrays.stream method or the Stream.of method.
         // While directly calling stream() on an array like you can on a collection (e.g., List) is not possible,
+//        String[] names = {"Apple", "Banana", "Avocado", "Cherry"};
+//        //Stream.of(names)
+//        Arrays.stream(names).filter(new Predicate<>() {
+//            @Override
+//            public boolean test(String s) {
+//                return s.startsWith("A");
+//            }
+//        }).collect(Collectors.toList());
+        //************* with lambda ****************//
+
         String[] names = {"Apple", "Banana", "Avocado", "Cherry"};
-        //Stream.of(names)
-        Arrays.stream(names).filter(new Predicate<String>() {
-            @Override
-            public boolean test(String s) {
-                return s.startsWith("A");
-            }
-        }).collect(Collectors.toList());
+        Arrays.stream(names).filter(s -> s.startsWith("A")).collect(Collectors.toList());
     }
 }
