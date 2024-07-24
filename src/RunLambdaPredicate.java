c2948f70c6d822207dacad6dcd36c4777d520ce3
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-24 11:51:50 +0330

    predicate and lambda expression

--- src/RunLambdaPredicate.java
+++ src/RunLambdaPredicate.java
@@ -1,2 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class RunLambdaPredicate {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Apple", "Banana", "Avocado", "Cherry");
+        names.stream().filter(s -> s.startsWith("A")).collect(Collectors.toList());
+    }
 }
