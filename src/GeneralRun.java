1e921082d4be8e820b46d6489c68a091336ba385
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-27 18:38:30 +0330

    when we specific which thread should have the highest priority, is just like a suggestion, but the OS scheduling look at the other factors, like which thread will take the lowest time.

--- src/GeneralRun.java
+++ src/GeneralRun.java
@@ -1,14 +1,11 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class GeneralRun {
     public static void main(String[] args) {
-      String[] names = {"Sarah" , "Ali" , "Alice" , "Fatemeh"};
-        Arrays.sort(names, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return 0;
-            }
-        });
+        ThreadA threadA = new ThreadA();
+        ThreadB threadB = new ThreadB();
+
+        threadB.setPriority(Thread.MAX_PRIORITY);
+
+        threadA.start();
+        threadB.start();
     }
 }
---
+++ src/ThreadA.java
@@ -0,0 +1,6 @@
+public class ThreadA extends Thread{
+    public void run(){
+        for(int i = 0; i<= 10 ; i++)
+            System.out.println("A");
+    }
+}
