267d67badab00a289fd3d228e6c302574d380e34
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-07-28 14:02:09 +0330

    instead of extends from Thread class, we can implement from Runnable class

--- src/RunnableClass.java
+++ src/RunnableClass.java
@@ -1,31 +1,21 @@
-import java.util.*;
-
 public class RunnableClass {
     public static void main(String[] args)
     {
+        Runnable runnableA = () -> {
+            for(int i = 0; i<= 10 ; i++)
+                System.out.println("My Thread A");
+        };
+        Runnable runnableB = () -> {
+            for(int i = 0; i<= 10 ; i++)
+                System.out.println("My Thread B");
+        };
 
-//        String s1 = new String("HI");
-//        String s2 = new String("HI");
-//        System.out.println(s1==s2);
-//        System.out.println(s1.equals(s2));
-        Student s1 = new Student(3 , "Negar");
-        Student s2 = new Student(3 , "Negar");
-        Student s3 = new Student(3 , "Negarr");
-        System.out.println(s1==s2);
-        System.out.println(s1.equals(s2));
-        System.out.println(s1.equals(s3));
-        System.out.println(s1.hashCode());
-        System.out.println(s2.hashCode());
+        Thread threadA = new Thread(runnableA);
+        Thread threadB = new Thread(runnableB);
 
-//        List<Student> list = new ArrayList<>();
-//        list.add(new Student(12 , "John"));
-//        list.add(new Student(9 , "Ali"));
-//        list.add(new Student(23 , "Mark"));
-//        list.add(new Student(11 , "Sarah"));
-//
-//        Collections.sort(list);
-//        for(Student i : list)
-//            System.out.println(i);
+        threadA.start();
+        threadB.start();
+
     }
 }
 
