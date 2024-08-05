b108d4b4a9b8ff1a931d2c954ddf8d251364846e
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-08-05 14:11:47 +0330

    the Operation of hashcode and equals methods

--- src/HashCodeExample.java
+++ src/HashCodeExample.java
@@ -2,8 +2,15 @@ public class HashCodeExample {
     public static void main(String[] args) {
         String a = "100";
         String b = "100";
+
+        int i = 90;
+        int j = 90;
+
         System.out.println("HashCode of a = " + a + ": " + a.hashCode());
         System.out.println("HashCode of b = " + b + ": " + b.hashCode());
+
+        System.out.println("HashCode of i = " + i + ": " + Integer.hashCode(i));
+        System.out.println("HashCode of j = " + j + ": " + Integer.hashCode(j));
 
         String c = "500";
         System.out.println("HashCode of c = " + c + ": " + c.hashCode());
