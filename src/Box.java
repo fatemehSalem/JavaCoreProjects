6d2e3bd16072c60f2f36547795c463d66f988541
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-08-10 09:10:42 +0330

    Generic class

--- src/Box.java
+++ src/Box.java
@@ -4,11 +4,15 @@ public class Box<T, V extends Number> {
 
             private T item;
             private V externalItem;
+            private String boxName;
 
-            public Box(T item, V externalItem){
+            public Box(T item, V externalItem , String boxName){
                 this.item = item;
                 this.externalItem = externalItem;
+                this.boxName = boxName;
             }
+
+
             public T getItem() {
                 return item;
             }
@@ -33,4 +37,21 @@ public class Box<T, V extends Number> {
             public static void addToList(List<? super Integer> list) {
                 list.add(123);
             }
-        }
+
+
+    public String getBoxName() {
+        return boxName;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public void setExternalItem(V externalItem) {
+        this.externalItem = externalItem;
+    }
+
+    public void setBoxName(String boxName) {
+        this.boxName = boxName;
+    }
+}
--- src/RunGenerics.java
+++ src/RunGenerics.java
@@ -3,8 +3,9 @@ import java.util.List;
 
 public class RunGenerics {
     public static void main(String[] args) {
-    Box<Integer , Double> box = new Box<>(10 , 20.08);
-    //System.out.println(box.multiItems());
+    Box<Integer , Double> box = new Box<>(10 , 20.08 , "Number 20");
+
+    System.out.println(box.getBoxName() + " , " + box.getExternalItem() + " , " + box.getItem());
 
     Integer[] intArray = {1, 2, 3, 4};
     Box.printArray(intArray);
