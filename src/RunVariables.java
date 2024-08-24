3622a069fc929990c9b9757a3ea3b9f81d0a888d
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-08-24 16:08:44 +0330

    flatMap

--- src/RunVariables.java
+++ src/RunVariables.java
@@ -2,9 +2,26 @@
 public class RunVariables {
 
     public static void main(String[] args) {
-         int x = 10;  // Stored in stack
-        RunVariables example = new RunVariables();  // Reference stored in stack, object stored in heap
-        example.display();
+       System.out.println("========== with new String============");
+
+        String s1= new String("Fatemeh");
+        String s2= new String("Fatemeh");
+        System.out.println(s1.equals(s2));
+        System.out.println(s1 == s2);
+
+        System.out.println("========== without new String============");
+
+        String s4= "Fatemeh";
+        String s5= "Fatemeh";
+        System.out.println(s4.equals(s5));
+        System.out.println(s4 == s5);
+
+        System.out.println("========== with new String ADD intern============");
+        String aS1 = s1.intern();
+        String aS2 = s2.intern();
+        System.out.println(aS1.equals(aS2));
+        System.out.println(aS1 == aS2);
+
     }
 
     public void display() {
