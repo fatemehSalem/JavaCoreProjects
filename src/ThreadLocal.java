7ba7c4ea4b97d97469204daa3ae04300ed32f132
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-08-25 17:33:18 +0330

    multi inheritance

---
+++ src/ThreadLocal.java
@@ -0,0 +1,11 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/*
+public class ThreadLocal {
+    ThreadLocal<SimpleDateFormat> dateFormat = ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd"));
+
+    public String formatDate(Date date) {
+        // Each thread gets its own instance of SimpleDateFormat
+        return dateFormat.get().format(date);
+    }
+}*/
