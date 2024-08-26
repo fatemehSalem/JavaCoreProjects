086eef701201936d152ddd269b170e37500e99a5
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-08-26 11:43:26 +0330

    multi inheritance

--- src/TreeSetExample.java
+++ src/TreeSetExample.java
@@ -6,8 +6,6 @@ import java.util.TreeSet;
 public class TreeSetExample {
     public static void main(String[] args) {
         TreeSet<Integer> treeSet1 = new TreeSet<>();
-        Set<Integer> treeSet2 = new TreeSet<>();
-        Collection<Integer> treeSet3 = new TreeSet<>();
 
         treeSet1.add(1);
         treeSet1.add(2);
@@ -18,8 +16,6 @@ public class TreeSetExample {
         Iterator<Integer> iterator = treeSet1.iterator();
         while (iterator.hasNext())
             System.out.println(iterator.next());
-
-
     }
 
 }
