b3d18715ae9dd4d1b842dff607c10d772ba61180
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2024-09-08 11:09:19 +0330

    YieldExample

--- src/YieldExample.java
+++ src/YieldExample.java
@@ -1,23 +1,23 @@
 public class YieldExample {
-/*    public static void main(String[] args) {
+   public static void main(String[] args) {
         Runnable runnable = () -> {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i < 3; i++) {
                 System.out.println(Thread.currentThread().getName() + " is executing.");
                 Thread.yield();
             }
         };
 
-        Thread t1 = new Thread(runnable, "Thread-1");
-        Thread t2 = new Thread(runnable, "Thread-2");
+        Thread t1 = new Thread(runnable, "Thread 1");
+        Thread t2 = new Thread(runnable, "Thread 2");
 
         t1.start();
         t2.start();
-    }*/
-    public static void main(String[] args) throws InterruptedException {
+    }
+/*    public static void main(String[] args) throws InterruptedException {
         Thread myThread = new Thread(() -> System.out.println("Hello from new thread"));
         myThread.start();
         Thread.yield();
         System.out.println("Hello from main thread");
         myThread.join();
-    }
+    }*/
 }
